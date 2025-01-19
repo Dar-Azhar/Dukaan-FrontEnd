6044f9f870220153ce460b18ea4cdf88f5465cb3
Azhar Ali <dev.darazhar@gmail.com>
2025-01-19 18:27:04 +0530

    updated cart page for book image

--- src/pages/books/CartPage.jsx
+++ src/pages/books/CartPage.jsx
@@ -48,7 +48,7 @@ const CartPage = () => {
                                                     <div className="h-24 w-24 flex-shrink-0 overflow-hidden rounded-md border border-gray-200">
                                                         <img
                                                             alt=""
-                                                            src={`${getImgUrl(product?.coverImage)}`}
+                                                            src={product?.coverImage}
                                                             className="h-full w-full object-cover object-center"
                                                         />
                                                     </div>
